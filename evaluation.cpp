09af8a612d520e6c87cdcca0e409644336bc7f94
Leon Eltscher <czermakleon@gmail.com>
2026-05-10 13:50:43 +0200

    Add evaluation logic and piece value tables for chess engine

---
+++ evaluation.cpp
@@ -0,0 +1,118 @@
+#include "evaluation.h"
+
+const int PIECE_VALUES[7] = {
+    100,  // PAWN
+    320,  // KNIGHT
+    330,  // BISHOP
+    500,  // ROOK
+    900,  // QUEEN
+    0,    // KING
+    0     // NONE
+};
+
+// Gibt jeder Figur einen Grundwert
+
+//Postionstabellen YAY
+
+const int PAWN_TABLE[64] = {
+     0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+     5,  5, 10, 25, 25, 10,  5,  5,
+     0,  0,  0, 20, 20,  0,  0,  0,
+     5, -5,-10,  0,  0,-10, -5,  5,
+     5, 10, 10,-20,-20, 10, 10,  5,
+     0,  0,  0,  0,  0,  0,  0,  0
+};
+
+const int KNIGHT_TABLE[64] = {
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+};
+
+const int BISHOP_TABLE[64] = {
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20
+};
+
+const int ROOK_TABLE[64] = {
+     0,  0,  0,  0,  0,  0,  0,  0,
+     5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+     0,  0,  0,  5,  5,  0,  0,  0
+};
+
+const int QUEEN_TABLE[64] = {
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+};
+
+const int KING_TABLE[64] = {
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+     20, 20,  0,  0,  0,  0, 20, 20,
+     20, 30, 10,  0,  0, 10, 30, 20
+};
+
+
+int getPieceTableValue(PieceType piece, Color color, int square) {
+    int idx = (color == WHITE) ? (63 - square) : square;
+
+    switch (piece) {
+        case PAWN:   return PAWN_TABLE[idx];
+        case KNIGHT: return KNIGHT_TABLE[idx];
+        case BISHOP: return BISHOP_TABLE[idx];
+        case ROOK:   return ROOK_TABLE[idx];
+        case QUEEN:  return QUEEN_TABLE[idx];
+        case KING:   return KING_TABLE[idx];
+        default:     return 0;
+    }
+}
+// Gibt den Wert der Position einer Figur zurück, abhängig von ihrer Art, Farbe und Position auf dem Brett
+
+int evaluateBoard(const Board& board) {
+    int score = 0;
+
+    for (int idx = 0; idx < 64; idx++) {
+        const Square& sq = board.squares[idx];
+        if (sq.isEmpty) continue;
+
+        int value = PIECE_VALUES[sq.piece]
+                  + getPieceTableValue(sq.piece, sq.color, idx);
+
+        if (sq.color == WHITE)
+            score += value;
+        else
+            score -= value;
+    }
+
+    return score;
+}
+// Berechnung des Gesamtwerts der Stellung
+
---
+++ evaluation.h
@@ -0,0 +1,8 @@
+#ifndef EVALUATION_H
+#define EVALUATION_H
+
+#include "move_validation.h"
+
+int evaluateBoard(const Board& board);
+
+#endif
